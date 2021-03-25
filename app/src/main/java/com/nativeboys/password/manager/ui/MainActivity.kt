086631b574489d35a81e9ce57e194791e5061352
Vaggelis95 <v.boudis1995@gmail.com>
2021-03-25 12:37:42 +0200

    TODOs applied

--- app/src/main/java/com/nativeboys/password/manager/ui/MainActivity.kt
+++ app/src/main/java/com/nativeboys/password/manager/ui/MainActivity.kt
@@ -8,5 +8,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /*TODO:
+        *  1. Regular Expression Weak-Medium-Strong Password
+        *  2. Confirmation Dialog
+        *  3. Category Thumbnail Chooser
+        *  4. Master Password Constructor Fragment
+        *  5. Master Password Fragment
+        *  6. Password Generator Fragment
+        *  7. Welcome Screen with basic Information about the app
+        *  8. Settings (exit, theme, patreon link)*/
     }
 }
